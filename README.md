716b95034c002a41cc01b2fb671bec8460f90f46
jleeau <jacky.lee.au@gmail.com>
2021-01-31 17:01:53 +1100

    fetch employee directory

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # birthdays-and-events-2
+## Overview
+This app takes employee's birthdays and special events from the holiday calendar and posts to a single channel in Slack.
+To be scheduled daily via Heroku.
+
+
+
+## TODO
+- Pull in birthdays, work anniversaries, holidays, annual leave (who's out?)
+- Configure daily message - birthdays, work anniversaries
+- Configure weekly message - public holidays, who's out
+- Build messaging helper
+- Connect to Slack channel
+- Build scheduler helper https://www.npmjs.com/package/node-schedule
+- Handle timezones for birthdays/work anniversaries
+- Build visual interface for turning on/off schedule
+- Push to Heroku
+
+- Edit link to Slack channel
+- Set custom schedules
+- 
+
+
+## Structure
+- main file
+- Fetch helper
+- BambooHR helper
+- Message helper
+- Slack helper
+- Schedule helper
+
+
+
 
 ## Steps
 ```
--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-console.log(`Test`);
+const fetch = require(`node-fetch`);
+
+require(`dotenv`).config();
+
+// Constants
+const bambooURL = `https://${process.env.BAMBOO_API_KEY}:x@api.bamboohr.com/api/gateway.php/${process.env.BAMBOO_SUBDOMAIN}/v1/`;
+
+
+
+
+console.log(`Starting...`);
+
+
+
+// Make request
+const requestHandler = async (url, fields) => {
+    try {
+        params = { fields: fields };
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+        let res = await fetch(url, {
+            method: `GET`,
+            headers: {
+                accept: `application/json`
+            }
+        });
+        
+        if (res.status === 200) {
+            return await res.json();
+        } else {
+            console.log(`Failed to fetch from BambooHR (${res.status}): ${res.headers.get('X-BambooHR-Error-Message')}`);
+            return null;       
+        }
+    } catch(err) {
+        console.log('Error encountered: ' + err);
+        return null;
+    }
+}
+
+
+let getEmployeeData = async(employeeId, fields = null) => {
+    try {
+        let url = new URL(bambooURL + `employees/` + employeeId);
+        return await requestHandler(url, fields);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+let main = async () => {
+    let dir = await getEmployeeData(`directory`);
+    console.log(dir);
+}
+
+main();
+
+// Birthdays
+
+// Work Anniversaries
+
+// Holidays
+
+// Annual Leave
+
+
